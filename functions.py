5abe2f5d33b6a5540c14868e256430692596dc80
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2015-07-16 15:18:27 +1000

    Outline.

---
+++ functions.py
@@ -0,0 +1,21 @@
+# functions.py: functions required for field data import gui
+
+# Global constants
+LABORATORY = "Field Measurement"
+DATA_SOURCE = "Field Data"
+# List of available instruments
+INSTRUMENTS = ["Hydrolab DS5"]
+
+# Check validity of the instrument file
+def check_file_validity(instrument):
+
+
+def get_sampling_time:
+
+
+def get_sampling_number:
+
+
+def get_fraction_number:
+
+
