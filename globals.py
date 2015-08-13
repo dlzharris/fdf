77020b8f12b9609d224dfa5e8c59474a63df0109
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2015-08-13 16:10:54 +1000

    First commit - global constants moved to this file.

--- globals.py
+++ globals.py
@@ -1,1 +1,10 @@
-__author__ = 'harrisdn'
+# Global constants
+LABORATORY = "Field Measurement"
+DATA_SOURCE = "Field Data"
+# List of available instruments
+INSTRUMENTS = ["Hydrolab DS5", "Hydrolab MS4", "Hydrolab MS5"]
+# List of available field officers
+FIELD_STAFF = ["Andy Wise", "Sarah McGeoch"]
+# Instrument names as variables
+hydrolab_instruments = ["Hydrolab DS5", "Hydrolab MS4", "Hydrolab MS5"]
+
