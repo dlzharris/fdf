377e9b3fdf6baf3de9291dfd0ecce8a2d3147597
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2015-08-20 09:58:16 +1000

    Boolean values list added (accidentally updated functions.py with same comment.

--- globals.py
+++ globals.py
@@ -9,6 +9,8 @@ FIELD_STAFF = ["Andy Wise", "Sarah McGeoch"]
 SAMPLE_TYPES = ["P", "RE", "DU", "TR", "QR", "QB", "QT"]
 # List of available matrices
 MATRIX_TYPES = ["ST", "SG", "GB", "GP", "PR"]
+# Boolean values
+BOOLEAN = ["Yes", "No"]
 
 # Instrument names as variables
 hydrolab_instruments = ["Hydrolab DS5", "Hydrolab MS4", "Hydrolab MS5"]
