13d560af93ac0d8b95e57bb49c8c3e29a826691b
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2015-08-31 11:28:57 +1000

    Added default instrument.

--- globals.py
+++ globals.py
@@ -3,6 +3,7 @@ LABORATORY = "Field Measurement"
 DATA_SOURCE = "Field Data"
 # List of available instruments
 INSTRUMENTS = ["Hydrolab DS5", "Hydrolab MS4", "Hydrolab MS5"]
+DEFAULT_INSTRUMENT = "Hydrolab DS5"
 # List of available field officers
 FIELD_STAFF = ["Andy Wise", "Sarah McGeoch"]
 # List of available sample types
