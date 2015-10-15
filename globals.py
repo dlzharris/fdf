496820fae63de620f23f8f95e64612e6416aebd2
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2015-10-16 10:00:33 +1100

    Drop-down selections updated.

--- globals.py
+++ globals.py
@@ -10,22 +10,46 @@ Date modified: 13/10/2015
 # Global constants
 LABORATORY = "Field Measurement"
 DATA_SOURCE = "Field Data"
+
 # List of available instruments
 INSTRUMENTS = ["Hydrolab DS5", "Hydrolab MS4", "Hydrolab MS5"]
 TURBIDITY_INSTRUMENT = ["HACH Tubiditimeter 2100P", "HACH Tubiditimeter 2100Q"]
 DEFAULT_INSTRUMENT = "Hydrolab DS5"
 DEFAULT_TURB_INSTRUMENT = "HACH Tubiditimeter 2100P"
-# List of available field officers
-FIELD_STAFF = ["Andy Wise", "Sarah McGeoch"]
+
 # List of available sample types
-SAMPLE_TYPES = ["P", "RE", "DU", "TR", "QR", "QB", "QT"]
+SAMPLE_TYPES = ["P", "R", "D", "T", "QR", "QB", "QT"]
+
 # List of available matrices
 MATRIX_TYPES = ["ST", "SG", "GB", "GP", "PR"]
+
 # Boolean values
 BOOLEAN = ["Yes", "No"]
+
 # List of available parameters
 PARAMETERS = ["conductivity_uncomp", "do", "do_sat", "gauge_height",
               "ph", "temp_c", "turbidity", "water_depth"]
+
+# List of available field officers
+FIELD_STAFF = ["Alison Lewis",
+               "Andy Wise",
+               "Claire Evans",
+               "Dan Poflotski",
+               "Dane Clarke",
+               "Darice Pepper",
+               "David Donnelly",
+               "Gerhard Schulz",
+               "Gordon Honeyman",
+               "James Hitchcock",
+               "Jane Rowlands",
+               "Maxine Rowley",
+               "Michael Rowe",
+               "Monika Muschal",
+               "Sarah McGeoch",
+               "Skye Taylor",
+               "Sue Botting",
+               "Tracy Fulford",
+               "Warwick Mawhinney"]
 
 # Instrument names as variables
 hydrolab_instruments = ["Hydrolab DS5", "Hydrolab MS4", "Hydrolab MS5"]
