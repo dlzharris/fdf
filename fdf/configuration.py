6188083414fed34bb4498e5008661d951e0789a8
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2016-09-28 11:47:27 +1000

    config files moved to config directory.

--- fdf/configuration.py
+++ fdf/configuration.py
@@ -1,6 +1,6 @@
 import yaml
 
 # Set up global configurations
-app_config = yaml.load(open('app_config.yaml').read())
-column_config = yaml.load(open('column_config.yaml').read())
-station_list = yaml.load(open('station_list.yaml').read())
+app_config = yaml.load(open('config/app_config.yaml').read())
+column_config = yaml.load(open('config/column_config.yaml').read())
+station_list = yaml.load(open('config/station_list.yaml').read())
