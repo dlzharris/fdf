d16aa8d7fbc48faa3f869db4a572a29e805c84a3
Daniel Harris <dlz.harris@gmail.com>
2017-10-16 14:43:59 +1100

    Versioning to 1.1.0

--- fdf/fdf.py
+++ fdf/fdf.py
@@ -41,7 +41,7 @@ __author__ = 'Daniel Harris'
 __date__ = '09 March 2017'
 __email__ = 'daniel.harris@dpi.nsw.gov.au'
 __status__ = 'Production'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 
 ###############################################################################
